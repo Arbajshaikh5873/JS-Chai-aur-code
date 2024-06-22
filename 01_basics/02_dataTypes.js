51210c176deca9c8e264aa0fc9159be950ada088
Arbajshaikh5873 <155817228+Arbajshaikh5873@users.noreply.github.com>
2024-06-22 05:55:42 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,42 @@
+"use strict";//treate all these code as newer version by this statement 
+
+// alert(3+3)//it generates notification on the top
+//but this alert is used only in browser not in nodejs
+
+let name ="Arbaj " //thisbis a string 
+let age = 18 //this is a number (integer) type 
+let isLoggedIn = false//this is boolean type 
+
+// another data types:-
+
+// 1) there is another data type used for largnumber #bigint 
+
+// 2) null :- standalone value 
+// 3) undefined 
+let state; //undefined  type 
+
+// 4)symbol :- used for uniquness 
+// 5) object :- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(typeof undefined)//op is undefined 
+
+console.log(typeof null)//op is object
