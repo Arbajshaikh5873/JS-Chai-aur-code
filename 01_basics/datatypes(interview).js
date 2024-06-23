741d70eafe6e7d476fbb48c100e709a430ea0763
Arbajshaikh5873 <155817228+Arbajshaikh5873@users.noreply.github.com>
2024-06-23 11:42:58 +0000

    datatype summary

---
+++ 01_basics/datatypes(interview).js
@@ -0,0 +1,58 @@
+// Primitive:- 
+// these are the datatypes which we can not directly modify
+// we modify the copy of original vaue remain same 
+
+
+/*  7 Types :-
+String 
+Number 
+Boolean 
+null
+undefined
+symbol
+BigInt
+*/
+
+//reference (Non Primitive )
+
+/*
+Array 
+Objects
+Functions
+*/
+
+// Array:- 
+
+const heros = ["shaktiman", "naagraj", "doga"];
+console.log(typeof heros);
+//object:-
+
+let myObj = {
+    name : "Arbaj",
+    age : 21
+}
+
+//Function:-
+
+const myFunction = function () {
+    console.log("Hello World");  
+}
+
+/*
+typeof 
+
+null => object 
+
+undefined => undefined
+
+Number => number 
+
+Arrays  =>  object
+
+Function  =>  function
+
+Object  =>  object
+
+*/
+ 
+
