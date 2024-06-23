ed60588a756d4e78aea2755c9919b35e8971f8ae
Arbajshaikh5873 <155817228+Arbajshaikh5873@users.noreply.github.com>
2024-06-23 06:03:53 +0000

    conversion of data types

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,7 +1,48 @@
-let score = "hitesh"
+let score = "33abc";//this is not a number this is strinng 
 
-console.log(typeof score);
+console.log(typeof score);//string 
 
-let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
+let valueInNumber = Number (score)//string is converte into number data type 
+console.log(typeof valueInNumber);//number 
+console.log(valueInNumber);//NaN(Not a number )
+/*if the vlue of score is undefined 
+i.e. let score = undefined;
+then also op of console.log(valueInNumber); is Nan
+*/
+
+//"33" => 33
+//"33abc" => NaN 
+//undefined => NaN 
+
+let isLoggedIn = 1;
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+op of console.log(booleanIsLoggedIn); when the value of let isLoggedIn   is 
+
+let isLoggedIn = 1 => true
+
+let isLoggedIn = 0 => false
+
+let isLoggedIn = null => false
+
+let isLoggedIn = "" => false
+
+
+let isLoggedIn = "hitesh" => true
+
+*/
+
+
+// conversion into the string 
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+
+console.log(typeof stringNumber);//string 
+console.log(stringNumber);//"33"
 
